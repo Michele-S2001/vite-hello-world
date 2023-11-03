52d5e14b508909a01c7a6311968599b6631c4f65
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-03 16:30:51 +0100

    ADD > task done

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+
+</script>
+
+<template>
+  <button @click="count++">
+    Clicked here {{ count }} times
+   </button>
+</template>
